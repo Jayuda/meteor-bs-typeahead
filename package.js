894e0711eb01836573a1268bb18892104c5fe4a3
comerc <comerc@mail.ru>
2015-01-08 00:52:43 +0300

    bump ver

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'comerc:bs-typeahead',
   summary: 'Typeahead-plugin with Bootstrap-style',
-  version: '1.0.0',
+  version: '1.0.1',
   git: 'https://github.com/comerc/meteor-bs-typeahead.git'
 });
 
