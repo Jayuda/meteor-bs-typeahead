2715fe7005028e2d4fa1ec1a49c7c7c8e98b01f8
comerc <comerc@mail.ru>
2015-01-15 21:27:30 +0300

    Update package.js

--- package.js
+++ package.js
@@ -1,12 +1,13 @@
 Package.describe({
   name: 'comerc:bs-typeahead',
-  summary: 'Typeahead-plugin with Bootstrap',
-  version: '1.0.1',
+  summary: 'Typeahead-plugin with Bootstrap-style',
+  version: '1.0.2',
   git: 'https://github.com/comerc/meteor-bs-typeahead.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.2.1');
+  api.use('jquery', 'client');
   api.addFiles([
     'typeahead.css',
     'typeahead.bundle.js',
